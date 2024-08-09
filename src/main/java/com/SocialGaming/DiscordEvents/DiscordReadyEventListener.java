1031eca2d13cce9a25f5af7c855d3e7c6c8aa5f5
jefferyrutherford <jefferyrutherford@outlook.com>
2024-08-08 22:13:19 -0500

    Adding creating a new game-night-admin role that is the only role that can access the new text channel

--- src/main/java/com/SocialGaming/DiscordEvents/DiscordReadyEventListener.java
+++ src/main/java/com/SocialGaming/DiscordEvents/DiscordReadyEventListener.java
@@ -1,7 +1,10 @@
 package com.SocialGaming.DiscordEvents;
 
 
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.PermissionOverride;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
@@ -10,6 +13,8 @@ import net.dv8tion.jda.api.hooks.EventListener;
 import org.jetbrains.annotations.NotNull;
 
 
+import java.awt.*;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
@@ -27,10 +32,16 @@ public class DiscordReadyEventListener implements EventListener {
 
     private void InitAdminChannel(List<Guild> guilds) {
         guilds.forEach(guild -> {
+            System.out.println(guild.getChannels());
             AtomicReference<Boolean> channelExists = new AtomicReference<>(false);
 
-            if (guild.getTextChannelsByName("Game Night Admin", false).isEmpty()) {
-                TextChannel textChannel = guild.createTextChannel("Game Night Admin").complete();
+            Role role = guild.createRole().setName("game-night-admin").setColor(Color.red).setHoisted(true).setMentionable(false).complete();
+
+            if (guild.getTextChannelsByName("game-night-admin", true).isEmpty()) {
+                TextChannel textChannel = guild.createTextChannel("game night admin")
+                        .addPermissionOverride(role, EnumSet.of(Permission.VIEW_CHANNEL),null)
+                        .addPermissionOverride(guild.getPublicRole(), null, EnumSet.of(Permission.VIEW_CHANNEL))
+                        .complete();
                 textChannel.sendMessage("Game Night Admin Created.").queue();
                 textChannel.sendMessage("Discord Server is connected to Game Night Bot").queue();
             }
