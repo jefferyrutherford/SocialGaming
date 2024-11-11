74b2299c9ad809d88665af0497828d71b776865f
jefferyrutherford <jefferyrutherford@outlook.com>
2024-11-10 22:15:07 -0600

    Adding More To the InitDB script

---
+++ src/main/resources/db/migration/V1__InitDB.sql
@@ -0,0 +1,39 @@
+create table user
+(
+    Email  varchar(100) not null
+        primary key,
+    UserID int auto_increment,
+    constraint user_pk2
+        unique (UserID)
+);
+
+create table game
+(
+    Title  varchar(100) not null
+        primary key,
+    Rating ENUM ('E','T','M'),
+    Genre  SET ('Shooter','BR','Other'),
+    GameID int auto_increment,
+    constraint game_pk
+        unique (GameID)
+);
+
+
+create table format
+(
+    Format   varchar(100)                   not null
+        primary key,
+    TeamSize int default 1                  null,
+    FormatID int auto_increment,
+    GameID   int                            not null,
+    constraint format_pk
+        unique (FormatID),
+    constraint format_game_GameID_fk
+        foreign key (GameID) references game (GameID)
+);
+
+
+
+INSERT INTO user (Email) VALUE ('jeffTest@gmail.com');
+INSERT INTO game (Title, Rating, Genre) VALUE ('COD BLOPS6', 'M','Shooter');
+INSERT INTO format(Format, TeamSize, GameID) VALUE ('2v2', 2, 1);
