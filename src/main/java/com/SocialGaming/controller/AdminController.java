1d74e6261a393f916ac69f92981ad3530041dcfa
Jeffery Rutherford <jefferyrutherford@outlook.com>
2025-06-16 20:00:16 -0600

    Adding Authentication To All Endpoints Other Than Public

---
+++ src/main/java/com/SocialGaming/controller/AdminController.java
@@ -0,0 +1,14 @@
+package com.SocialGaming.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello Admin World";
+    }
+}
---
+++ src/main/java/com/SocialGaming/controller/PublicController.java
@@ -0,0 +1,14 @@
+package com.SocialGaming.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+    @GetMapping("/test")
+    public String test() {
+        return "Hello Admin World";
+    }
+}
